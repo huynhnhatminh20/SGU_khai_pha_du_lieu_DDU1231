b90f833ff5e2fb48f0044d3e73b33096ce0e00cc
huynhnhatminh20 <huynhnhatminh20102005@gmail.com>
2025-09-28 22:22:59 +0700

    Remove lesson1 from root, keep demo/lesson1

--- lesson1/ex3_circle.py
+++
@@ -1,9 +0,0 @@
-import math
-
-r = float(input("Nhập bán kính r: "))
-cv = 2 * math.pi * r
-dt = math.pi * r**2
-
-print(f"Bán kính: {r}")
-print(f"Chu vi hình tròn = {cv}")
-print(f"Diện tích hình tròn = {dt}")
